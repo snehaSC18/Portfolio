24f760bd9508605879cda5ef8d0702673d517e0c
snehaSc18 <snehachatt23@gmail.com>
2025-04-07 21:46:55 +0530

    Updated files with new changes

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-    
- 
-}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const app = document.getElementById('typewriter');
+
+  if (app) {
+      const typewriter = new Typewriter(app, {
+          loop: true,
+          delay: 75
+      });
+
+      typewriter
+          .pauseFor(500)
+          .typeString("Hi, I'm Sneha Chattoraj")
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString("Welcome to my portfolio 💻")
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString("I am a CSE Undergrad")
+          .pauseFor(1000)
+          .start();
+  }
+});
